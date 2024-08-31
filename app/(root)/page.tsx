b9d285303551b91f2a8dc311f6912e8339282825
naman rathore <namanrathore59@gmail.com>
2024-08-31 16:35:38 +0000

    update from gitpod

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
+import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
-    <Button>hello</Button>
+    <main className='home-container'>
+      <Header className='sticky top-0 left-0'>
+        <div className='flex items-center gap-2 lg:gap-4'>
+          notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+    </main>
   )
 }
 
--- lib/actions/room.action.ts
+++ lib/actions/room.action.ts
@@ -1,14 +1,36 @@
-import {nanoid} from "nanoid"
+"use client"
 
-export const createDocument = async ({ userId, email }: { CreateDocumentParams }) => {
+
+import { nanoid } from "nanoid"
+import { liveblocks } from "../liveblocks";
+import { revalidatePath } from "next/cache";
+import { parseStringify } from "../utils";
+
+export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
     const roomId = nanoid();
 
 
     try {
         const metadata = {
             createId: userId,
-            
+            email,
+            title: "Untitle"
+
         }
+
+        const usersAccesses: RoomAccesses = {
+            [email]: ["room:write"]
+        }
+
+
+
+        const room = await liveblocks.createRoom(roomId, {
+            metadata, usersAccesses, defaultAccesses: []
+        });
+
+        revalidatePath('/');
+        return parseStringify(room)
+
     } catch (error) {
         console.log(`error happen while creating room ${error}`)
     }
