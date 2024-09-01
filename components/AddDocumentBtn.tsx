e2286df20c3a31b5114f48fd01180f0d8e3c21b0
naman rathore <namanrathore59@gmail.com>
2024-09-01 12:47:45 +0000

    active collbe added

--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -8,25 +8,25 @@ import { useRouter } from 'next/navigation';
 
 const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
 
-const router=useRouter()
+    const router = useRouter()
 
-        const addDocHandler = async() => {
-    try {
-        const room = await createDocument({ userId, email })
-        // if(room) router.push(`/documents/room/${room.id}`)
-            if(room) router.push(`/documents/${room.id}`);
+    const addDocHandler = async () => {
+        try {
+            const room = await createDocument({ userId, email })
+            // if(room) router.push(`/documents/room/${room.id}`)
+            if (room) router.push(`/documents/${room.id}`);
 
-    } catch (error) {
-        console.log(error);
-    }
+        } catch (error) {
+            console.log(error);
         }
-  return (
-      <Button type='submit' onClick={addDocHandler} className='gradient-blue gap-1 flex shadow-md '>
+    }
+    return (
+        <Button type='submit' onClick={addDocHandler} className='gradient-blue gap-1 flex shadow-md'>
 
-       <Image src="/assets/icons/add.svg" alt='add' width={10} height={10}/>
-                 <p className='hidden sm:block'>Start a black document</p>
-       </Button>
-  )
+            <Image src="/assets/icons/add.svg" alt='add' width={10} height={10} />
+            <p className='hidden sm:block'>Start a black document</p>
+        </Button>
+    )
 }
 
 export default AddDocumentBtn
